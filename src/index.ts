72f6ae41ffb0474fbd256750f5c803ebf2f62208
Emosans <shreyasd1414@gmail.com>
2024-12-07 13:02:41 +0530

    :truck: added swagger

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import userRouter from "./routers/userRoute";
 import adminRouter from "./routers/adminRoute";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app: Application = express();
 app.use(express.json());
 dotenv.config();
 
+const swaggerDocument = YAML.load("./swagger.yaml");
 const port: number = 5000;
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
